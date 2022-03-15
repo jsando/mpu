85084b676e8224f41171e6afc19211968cbfa946
jsando <jason.sando.lv@gmail.com>
2022-03-15 10:10:13 -0700

    Add apache2 license.

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
+// Copyright 2022 Jason Sando <jason.sando.lv@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"flag"
 	"fmt"
-	"github.com/jsando/mpu/asm"
-	"github.com/jsando/mpu/machine"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
+	"github.com/jsando/mpu/asm"
+	"github.com/jsando/mpu/machine"
 )
 
 func main() {
